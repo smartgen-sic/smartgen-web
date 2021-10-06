54665160ba1b9595742a2ed8f2b3c8c6be34d2b8
elisnf <elisnurfadilah3@gmail.com>
2021-10-06 13:54:57 +0700

    udah

--- src/components/artikel/index.js
+++ src/components/artikel/index.js
@@ -38,60 +38,57 @@ export default function Artikel() {
 
             <div>
                 <a
-                    href="https://www.halodoc.com/artikel"
+                    href="http://upbk.unp.ac.id/news/read/22/cara-bergaul-yang-baik-agar-anda-punya-banyak-teman"
                     className="card-artikel2"
                 >
                     <div className="judul-artikel">
-                        <p>Curhat Bisa Membantu Kesehatan Mental</p>
+                        <p>Cara Bergaul Yang Baik Agar Anda Punya Banyak Teman</p>
                     </div>
                     <div className="isi-artikel">
                         <p>
-                            Menjaga kesehatan mental bisa dilakukan dengan
-                            beberapa cara, salah satunya......
+                        Karakter terbentuk karena kebiasaan, lingkungan, keturunan dan......
                         </p>
                     </div>
                     <div className="link-artikel">
-                        <p>source : https://www.halodoc.com/artikel/</p>
+                        <p>source : http://upbk.unp.ac.id/</p>
                     </div>
                 </a>
             </div>
 
             <div>
                 <a
-                    href="https://www.halodoc.com/artikel"
+                    href="https://www.cnnindonesia.com/gaya-hidup/20190715174718-284-412324/cara-mengatur-waktu-kerja-agar-lebih-produktif"
                     className="card-artikel3"
                 >
                     <div className="judul-artikel">
-                        <p>Curhat Bisa Membantu Kesehatan Mental</p>
+                        <p>Cara Mengatur Waktu Kerja Agar Lebih Produktif</p>
                     </div>
                     <div className="isi-artikel">
                         <p>
-                            Menjaga kesehatan mental bisa dilakukan dengan
-                            beberapa cara, salah satunya......
+                            Produktifitas kerja acapkali terhambat akibat manajer waktu yang buruk. Padahal......
                         </p>
                     </div>
                     <div className="link-artikel">
-                        <p>source : https://www.halodoc.com/artikel/</p>
+                        <p>source : https://www.cnnindonesia.com/</p>
                     </div>
                 </a>
             </div>
 
             <div>
                 <a
-                    href="https://www.halodoc.com/artikel"
+                    href="https://www.kejarmimpi.id/lakukan-kebiasaan-ini-untuk-kejarmimpi-membentuk-mental-yang-kuat.html"
                     className="card-artikel4"
                 >
                     <div className="judul-artikel">
-                        <p>Curhat Bisa Membantu Kesehatan Mental</p>
+                        <p>Lakukan kebiasaan ini untuk membentuk mental kuat</p>
                     </div>
                     <div className="isi-artikel">
                         <p>
-                            Menjaga kesehatan mental bisa dilakukan dengan
-                            beberapa cara, salah satunya......
+                        Hidup ini berliku-liku. Adakah hidup seseorang yang selalu lancar dan......
                         </p>
                     </div>
                     <div className="link-artikel">
-                        <p>source : https://www.halodoc.com/artikel/</p>
+                        <p>source : https://www.kejarmimpi.id/</p>
                     </div>
                 </a>
             </div>
